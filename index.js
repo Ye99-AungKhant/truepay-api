96482b46d31f97de899c2d8a36ae938c5c3e076d
Ye99-AungKhant <yeaungkhant077@gmail.com>
2024-08-30 00:16:59 +0630

    port

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 app.use('/user', userRouter)
 app.use('/transfer', transferRouter)
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
